099beea2ad36496f2d7e24b322bf68d56fee7719
Wang Jun Yan <wjy@e-teng.com.cn>
2018-10-10 18:22:38 +0800

    qs
    Change-Id: I55f92ea4fa3beafef679fdd58470e041c1267ede

--- src/com/wjy/redis/RedisUtil.java
+++ src/com/wjy/redis/RedisUtil.java
@@ -4,6 +4,18 @@ import redis.clients.jedis.Jedis;
 
 public class RedisUtil {
 
+	public static void set(String key, String value, int seconds) {
+
+		Jedis jedis = JedisPoolUtil.getInstance().getResource();
+
+		jedis.set(key, value);
+
+		jedis.expire(key, seconds);
+
+		jedis.close();
+
+	}
+
 	public static void set(String key, String value) {
 
 		Jedis jedis = JedisPoolUtil.getInstance().getResource();
