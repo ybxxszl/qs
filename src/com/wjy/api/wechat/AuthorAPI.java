f3901baab7d7ea609d6272bdefc6b72222d9a43c
Wang Jun Yan <wjy@e-teng.com.cn>
2018-10-16 16:36:15 +0800

    qs
    Change-Id: If09bdcaa7171805960181757f496951020a55c29

---
+++ src/com/wjy/api/wechat/AuthorAPI.java
@@ -0,0 +1,28 @@
+package com.wjy.api.wechat;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+
+import com.wjy.dao.AuthorDao;
+import com.wjy.log.LOG;
+
+@Path(value = "/author")
+@Produces(value = "application/json;charset=utf-8")
+public class AuthorAPI {
+
+	private AuthorDao authorDao = new AuthorDao();
+
+	@GET
+	@Path(value = "/getAuthor")
+	public Response getAuthor(@QueryParam(value = "code") String code) throws Exception {
+
+		LOG.pInfo("code: " + code);
+
+		return null;
+
+	}
+
+}
---
+++ src/com/wjy/util/WeChatUtil.java
@@ -0,0 +1,11 @@
+package com.wjy.util;
+
+public class WeChatUtil {
+
+	public void getCode2Session(String code) {
+
+		String url = "https://api.weixin.qq.com/sns/jscode2session?appid=APPID&secret=SECRET&js_code=JSCODE&grant_type=authorization_code";
+
+	}
+
+}
