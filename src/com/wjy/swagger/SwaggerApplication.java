b7fff1dd99909e33ee34c74ea4edc1ff3ea86d4e
Wang Jun Yan <wjy@e-teng.com.cn>
2019-01-08 14:41:14 +0800

    qs

--- src/com/wjy/swagger/SwaggerApplication.java
+++ src/com/wjy/swagger/SwaggerApplication.java
@@ -1,78 +1,44 @@
 package com.wjy.swagger;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-import org.apache.log4j.Logger;
-
-import com.wjy.exception.business.BusinessExceptionHandle;
-import com.wjy.exception.system.SystemExceptionHandle;
-import com.wjy.filter.RequestFilter;
 import com.wjy.util.PropertiesUtil;
 
 import io.swagger.jaxrs.config.BeanConfig;
 
+/**
+ * 使用Swagger暴露接口，需要使用@Provider注解需要扫描的工具类，例如请求过滤和异常处理，不需要使用singletons
+ * 
+ * @author wjy
+ * @date 2019年1月8日
+ */
 @ApplicationPath(value = "/api")
 public class SwaggerApplication extends Application {
 
-	private static final Logger LOGGER = Logger.getLogger(SwaggerApplication.class);
+	private static final boolean IS = Boolean.parseBoolean(PropertiesUtil.getValue("swagger.is"));
+	private static final String DESCRIPTION = PropertiesUtil.getValue("swagger.description");
+	private static final boolean SCAN = Boolean.parseBoolean(PropertiesUtil.getValue("swagger.scan"));
+	private static final String RESOURCEPACKAGE = PropertiesUtil.getValue("swagger.resourcePackage");
+	private static final String BASEPATH = PropertiesUtil.getValue("swagger.basePath");
+	private static final String[] SCHEMES = new String[] { PropertiesUtil.getValue("swagger.schemes") };
 
-	private Set<Object> singletons = new HashSet<Object>();
-
-	/*
-	 * 添加需要扫描的类
-	 */
 	public SwaggerApplication() {
-
-		// 请求过滤
-		singletons.add(new RequestFilter());
-
-		// 业务异常处理
-		singletons.add(new BusinessExceptionHandle());
-		// 系统异常处理
-		singletons.add(new SystemExceptionHandle());
-
-		// 微信
-		singletons.add(new com.wjy.api.wechat.AuthorAPI());
-		singletons.add(new com.wjy.api.wechat.DesignTempletAPI());
-		// 网站
-		singletons.add(new com.wjy.api.website.AuthorAPI());
-		singletons.add(new com.wjy.api.website.DesignTempletAPI());
-
-		// Swagger
-		singletons.add(new com.wjy.api.swagger.SwaggerAPI());
-
-		LOGGER.info("RestEasy配置加载完成 ！！！");
 
 		/*
 		 * Swagger配置
 		 */
-		boolean isSwagger = Boolean.parseBoolean(PropertiesUtil.getValueOrDefault("swagger.is", "true"));
-
-		if (isSwagger) {
+		if (IS) {
 
 			BeanConfig beanConfig = new BeanConfig();
 
-			beanConfig.setScan(Boolean.parseBoolean(PropertiesUtil.getValueOrDefault("swagger.scan", "true")));
-			beanConfig.setVersion(PropertiesUtil.getValueOrDefault("swagger.version", "1.0"));
-			beanConfig.setDescription(PropertiesUtil.getValueOrDefault("swagger.description", "Swagger API"));
-			beanConfig.setResourcePackage(PropertiesUtil.getValueOrDefault("swagger.resourcePackage", "com"));
-			beanConfig.setBasePath(PropertiesUtil.getValueOrDefault("swagger.basePath", "/api"));
-			beanConfig.setSchemes(new String[] { "http" });
-
-			LOGGER.info("Swagger配置加载完成 ！！！");
+			beanConfig.setDescription(DESCRIPTION);
+			beanConfig.setScan(SCAN);
+			beanConfig.setResourcePackage(RESOURCEPACKAGE);
+			beanConfig.setBasePath(BASEPATH);
+			beanConfig.setSchemes(SCHEMES);
 
 		}
-
-	}
-
-	@Override
-	public Set<Object> getSingletons() {
-
-		return singletons;
 
 	}
 
